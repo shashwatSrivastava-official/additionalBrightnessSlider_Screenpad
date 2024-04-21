d2666d318ab4487cc937b28dfdfe031702c7d777
Jaskaran Singh <44025944+jksjaz@users.noreply.github.com>
2024-04-21 15:53:19 -0500

    updated

--- extension.js
+++ extension.js
@@ -19,65 +19,66 @@
 /* exported init */
 "use strict";
 
-const { St, Clutter, Gio } = imports.gi;
+const { GObject, St, Clutter, Gio } = imports.gi;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 const Slider = imports.ui.slider;
 const ExtensionUtils = imports.misc.extensionUtils;
-
 const Util = imports.misc.util;
 
-class BrightnessIndicator extends PanelMenu.Button {
-  _init() {
-    super._init(0.0, _("Monitor Brightness"));
+var BrightnessIndicator = GObject.registerClass(
+  class BrightnessIndicator extends PanelMenu.Button {
+    _init() {
+      super._init(0.0, _("Monitor Brightness"));
 
-    const icon = new St.Icon({
-      icon_name: "display-brightness-symbolic",
-      style_class: "system-status-icon",
-    });
+      const icon = new St.Icon({
+        icon_name: "display-brightness-symbolic",
+        style_class: "system-status-icon",
+      });
 
-    this.add_child(icon);
+      this.add_child(icon);
 
-    this._sliderItem = new PopupMenu.PopupSliderMenuItem(0.5);
-    this._sliderItem.connect("value-changed", this._sliderChanged.bind(this));
-    this.menu.addMenuItem(this._sliderItem);
+      this._sliderItem = new PopupMenu.PopupSliderMenuItem(0.5);
+      this._sliderItem.connect("value-changed", this._sliderChanged.bind(this));
+      this.menu.addMenuItem(this._sliderItem);
 
-    this._updateBrightness();
-  }
-
-  _getMaxBrightness() {
-    let [success, stdout, stderr] = GLib.spawn_command_line_sync(
-      "brightnessctl -d DP-1 max"
-    );
-    if (!success) {
-      log(`Error getting max brightness: ${stderr}`);
-      return 100; // Default to 100 if there is an error
+      this._updateBrightness();
     }
-    return parseInt(stdout.toString().trim());
-  }
 
-  _updateBrightness() {
-    Util.spawnCommandLineAsyncIO(
-      "brightnessctl -d DP-1 get",
-      (stdout, stderr) => {
-        if (stderr !== "") {
-          log(`Error reading brightness: ${stderr}`);
-          return;
+    _updateBrightness() {
+      Util.spawnCommandLineAsyncIO(
+        "brightnessctl -d DP-1 get",
+        (stdout, stderr) => {
+          if (stderr !== "") {
+            log(`Error reading brightness: ${stderr}`);
+            return;
+          }
+          const maxBrightness = this._getMaxBrightness();
+          const currentBrightness = parseInt(stdout);
+          this._sliderItem.setValue(currentBrightness / maxBrightness);
         }
-        const maxBrightness = this._getMaxBrightness();
-        const currentBrightness = parseInt(stdout);
-        this._sliderItem.setValue(currentBrightness / maxBrightness);
-      }
-    );
-  }
+      );
+    }
 
-  _sliderChanged(slider, value) {
-    const maxBrightness = this._getMaxBrightness();
-    const brightnessValue = Math.round(value * maxBrightness);
-    Util.spawnCommandLine(`brightnessctl -d DP-1 set ${brightnessValue}%`);
+    _getMaxBrightness() {
+      let [success, stdout, stderr] = GLib.spawn_command_line_sync(
+        "brightnessctl -d DP-1 max"
+      );
+      if (!success) {
+        log(`Error getting max brightness: ${stderr}`);
+        return 100; // Default to 100 if there is an error
+      }
+      return parseInt(stdout.toString().trim());
+    }
+
+    _sliderChanged(slider, value) {
+      const maxBrightness = this._getMaxBrightness();
+      const brightnessValue = Math.round(value * maxBrightness);
+      Util.spawnCommandLine(`brightnessctl -d DP-1 set ${brightnessValue}%`);
+    }
   }
-}
+);
 
 var brightnessIndicator;
 
